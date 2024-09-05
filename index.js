4fb672dd8d929466966bf9f5ef1afa29822b3025
Devashish Priyadarshi <devashish234073@gmail.com>
2024-09-05 08:27:35 +0530

    added code to cast media to device

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var mdns = require('mdns-js');
 const http = require("http");
 const fs = require("fs");
+var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+var Client = require('castv2-client').Client;
 let obj = {};
 //if you have another mdns daemon running, like avahi or bonjour, uncomment following line
 //mdns.excludeInterface('0.0.0.0');
 
 //var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+let notConnected = true;
 function scan() {
     var browser = mdns.createBrowser(mdns.tcp('googlecast'));
 
@@ -17,13 +20,48 @@ function scan() {
     browser.on('update', function (data) {
         obj[data.type[0]["name"]] = data;
         console.log(`${data.addresses} ${data.fullname} ${data.type[0]["name"]}`);
+        if (notConnected && data.type[0]["name"] == "googlecast") {
+            notConnected = false;
+            var client = new Client();
+
+            client.connect(data.addresses[0], function () {
+                console.log("connected..");
+                client.launch(DefaultMediaReceiver, function (err, player) {
+                    if (err) {
+                        console.log("Unable to get player", err);
+                    } else {
+                        //console.log(player);
+                        var media = {
+                            contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
+                            contentType: 'video/mp4',
+                            streamType: 'BUFFERED',
+                            metadata: {
+                                type: 0,
+                                metadataType: 0,
+                                title: "Big Buck Bunny",
+                                images: [
+                                    { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg' }
+                                ]
+                            }
+                        };
+                        player.load(media, { autoplay: true }, function(err, status) {
+                            if(err) {
+                                console.log("Unable to load player",err);
+                            } else {
+                                console.log("Player loaded successfully",status);
+                            }
+                        });
+                    }
+                });
+            });
+        }
     });
 }
 
 scan();
 
-function serverFunc(req,res) {
-    if(req.url=="/api") {
+function serverFunc(req, res) {
+    if (req.url == "/api") {
         res.end(JSON.stringify(obj));
     } else {
         let html = fs.readFileSync("index.html");
@@ -33,6 +71,6 @@ function serverFunc(req,res) {
 
 let server = http.createServer(serverFunc);
 const PORT = 9999;
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
